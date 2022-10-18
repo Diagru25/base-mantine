8e7b3a925516230ed59fa7d8143bee0808f051fe
diagru <diagru2510@gmail.com>
2022-10-18 16:18:13 +0700

    [create] create login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,79 @@
+import {
+  Avatar,
+  Stack,
+  Text,
+  Input,
+  Button,
+  useMantineTheme,
+  Center,
+  PasswordInput,
+} from "@mantine/core";
 import { FC } from "react";
+import { IconStar } from "@tabler/icons";
+import styles from "pages/Login/style.module.css";
+import { Link } from "react-router-dom";
 
 const Login: FC = () => {
-  return <div>Login</div>;
+  const theme = useMantineTheme();
+  return (
+    <Stack align="center" className={styles.wrapper}>
+      <Avatar color="blue" radius="xl" size={48} mb="md">
+        <IconStar size={24} />
+      </Avatar>
+      <Text size={24}>Đăng nhập</Text>
+      <Stack
+        className={styles.form}
+        style={{
+          padding: "1rem",
+          backgroundColor: theme.colors.gray[0],
+          borderRadius: "6px",
+          border: "solid 1px",
+          borderColor: theme.colors.gray[3],
+        }}
+      >
+        <Input.Wrapper id="email" label="Username hoặc email" error="">
+          <Input id="email" placeholder="Your email" />
+        </Input.Wrapper>
+        <PasswordInput placeholder="Password" label="Mật khẩu" withAsterisk />
+        <Button>Đăng nhập</Button>
+      </Stack>
+      <Center
+        className={styles.form}
+        style={{
+          padding: "1rem",
+          borderRadius: "6px",
+          border: "solid 1px",
+          borderColor: theme.colors.gray[3],
+        }}
+      >
+        <Text size="sm">
+          Chưa có tài khoản?{" "}
+          <Text
+            className={styles.link}
+            component={Link}
+            to={"/"}
+            color="blue.5"
+            ml="xs"
+          >
+            Đăng ký ngay
+          </Text>
+        </Text>
+      </Center>
+      <Center
+        style={{ fontSize: 12, color: theme.colors.blue[5], marginTop: "2rem" }}
+      >
+        <Text className={styles.link} mr="sm" component={Link} to="/">
+          Terms
+        </Text>
+        <Text className={styles.link} mr="sm" component={Link} to="/">
+          Privacy
+        </Text>
+        <Text className={styles.link} mr="sm" component={Link} to="/">
+          Security
+        </Text>
+      </Center>
+    </Stack>
+  );
 };
 
 export default Login;
